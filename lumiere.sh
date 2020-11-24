6792bb084818e70fc47ba6df56774c3ff56afc55
EnzoALB <enzo.Albouy0@gmail.com>
2020-11-24 14:06:44 +0100

    Je me suis ammusé avec zenity

--- lumiere.sh
+++ lumiere.sh
@@ -26,9 +26,11 @@ then
 
 	state1=$(curl $Amp1/status --silent|jq .meters[0].power)
 	state2=$(curl $Amp2/status --silent|jq .meters[0].power)
-	INT=$(zenity --entry --text "Votre ampoule 1 consomme : $state1 et votre ampoule 2 consomme : $state2 Entrez l'état souhaité (on - off) :")
-	curl $Amp1/relay/0?turn=$INT --silent|jq
-	curl $Amp2/relay/0?turn=$INT --silent|jq
+	INT=$(zenity --forms --title="Lumières" --text="Votre ampoule 1 consomme : $state1 et votre ampoule 2 consomme : $state2" --add-entry="Ampoule 1 (on / off)" --add-entry="Ampoule 2 (on / off)")
+	INT1=$(echo $INT|cut -d "|" -f 1)
+	INT2=$(echo $INT|cut -d "|" -f 2)
+	curl $Amp1/relay/0?turn=$INT1 --silent|jq
+	curl $Amp2/relay/0?turn=$INT2 --silent|jq
 fi
 
 
