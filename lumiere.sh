e9509e07b59658ccb340af04ca064a5e80dbf44e
EnzoALB <enzo.Albouy0@gmail.com>
2020-11-24 13:34:32 +0100

    Script fini pour 2 ampoules avec BDD ampoules

--- lumiere.sh
+++ lumiere.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-state=$(curl 10.202.255.253/status --silent|jq .meters[0].power)
+Ampoule=$(zenity --entry --text "Quelle(s) ampoule(s) souhaitez vous utiliser ? (1 / 2 / all)")
 
-echo $state
+Amp1=$(cat Ampoules|grep Amp1| awk '{print $2}')
+Amp2=$(cat Ampoules|grep Amp2| awk '{print $2}')
 
-INT=$(zenity --entry --text "Votre ampoule consomme : $state Entrez l'état souhaité (on - off) :")
-curl 10.202.255.253/relay/0?turn=$INT --silent|jq
+if [ $Ampoule = 1 ]
+then
+
+	state=$(curl $Amp1/status --silent|jq .meters[0].power)
+	INT=$(zenity --entry --text "Votre ampoule consomme : $state Entrez l'état souhaité (on - off) :")
+	curl $Amp1/relay/0?turn=$INT --silent|jq
+fi
+
+if [ $Ampoule = 2 ]
+then
+
+	state=$(curl $Amp2/status --silent|jq .meters[0].power)
+	INT=$(zenity --entry --text "Votre ampoule consomme : $state Entrez l'état souhaité (on - off) :")
+	curl $Amp2/relay/0?turn=$INT --silent|jq
+fi
+
+if [ $Ampoule = all ]
+then
+
+	state1=$(curl $Amp1/status --silent|jq .meters[0].power)
+	state2=$(curl $Amp2/status --silent|jq .meters[0].power)
+	INT=$(zenity --entry --text "Votre ampoule 1 consomme : $state1 et votre ampoule 2 consomme : $state2 Entrez l'état souhaité (on - off) :")
+	curl $Amp1/relay/0?turn=$INT --silent|jq
+	curl $Amp2/relay/0?turn=$INT --silent|jq
+fi
+
+
+
+
+
